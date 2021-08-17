d80d3466dbd0d8a2f04c07ba7af9f5bedcdf7759
jessan <jessenia.garcia998@gmail.com>
2021-08-17 04:30:25 -0600

    writeFile added to generateMarkDown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,22 @@
 const fs = require('fs');
-const { title, features } = require('process');
 
+const writeFile = fileContent => {
+  return new Promise((resolve, reject) => {
+      fs.writeFile('.dist/README.md', fileContent, err => {
+          //if there's an error, reject the promise and send the errors to the Promise's `.catch()` method
+          if (err) {
+              reject(err);
+              //return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+              return;
+          }
+
+          //if everything went well, resolve the Promise and send the successful data to the `.then()` method
+          resolve({
+              ok: true,
+              message: 'README created!'
+          });
+      });
+  });
+};
+
+module.exports = {writeFile};
