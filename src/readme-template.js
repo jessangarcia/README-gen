74140bc1e4094224ef4fbae28f5a58adf0b40b45
jessan <jessenia.garcia998@gmail.com>
2021-08-17 04:23:25 -0600

    template added, if statements for some parts added

--- src/readme-template.js
+++ src/readme-template.js
@@ -1,11 +1,73 @@
-const inquirer = require('inquirer');
-
-const questions = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message: 'Enter project title.',
-        },
-    ]);
-};
+//creates the optional sections 
+const generateCredit = credText => {
+    if (!credText) {
+      return '';
+    }
+  
+    return `
+    ## Credit
+    ${credits}
+    `;
+}
+  
+const generateFeats = featText => {
+    if (!featText) {
+      return '';
+    }
+  
+    return `
+    ## Features
+    ${features}
+    `;
+}
+  
+const generateTest = testText => {
+    if (!testText) {
+      return '';
+    }
+  
+    return `
+    ## Test
+    ${tests}
+    `;
+}
+  
+module.exports = readArr => {
+    return `
+    # ${title} 
+    ![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg) Check out the badges hosted by [shields.io](https://shields.io/).
+  
+    ## Description
+    ${description}
+    
+    ## Table of Contents
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Credits](#credits)
+    * [Questions](#questions)
+    
+    ## Installation 
+    *Steps required to install project and how to get the development environment running:*
+    ${installation}
+    
+    ## Usage
+    ${usage}
+  
+    ${generateCredit}
+    
+    ## License
+    > This project is using the ${license} license.
+  
+    ${generateFeats}
+    
+    ## Contributing
+    ${contribution}
+  
+    ${generateTest}
+    
+    ## Questions
+    * ${github}
+    * ${email}
+    `
+}
